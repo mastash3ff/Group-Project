cd0790ad74c4c3fdec0e203c616f7d1e85a9b54a
mastash3ff <bsheffield2008@yahoo.com>
2013-07-20 20:23:58 -0500

    Added News Button. Another activity. Setting up RSS Feed.
    ...still in works.

--- GroupProject/src/com/example/groupproject/MainActivity.java
+++ GroupProject/src/com/example/groupproject/MainActivity.java
@@ -6,46 +6,48 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.webkit.WebView;
 import android.widget.Button;
 
 public class MainActivity extends Activity
 {
-  private Button entertainmentBtn,
-                 beachLocationsBtn,
-                 hotelsBtn,
-                 restaurantsBtn,
-                 hospitalsBtn,
-                 shoppingBtn;
-	
+	private Button entertainmentBtn,
+	beachLocationsBtn,
+	hotelsBtn,
+	restaurantsBtn,
+	hospitalsBtn,
+	shoppingBtn,
+	newsBtn;	
+
 	/**  Guide Lines and ideas*/
-  
+
 	//create listener buttons for categories - about 5 categories
-	  //possibly spawn new activities as a result from button clicks to open more selections
-	
+	//possibly spawn new activities as a result from button clicks to open more selections
+
 	//add google maps for locations; all else fails, open web page to google map location.
-	
+
 	//add simple animations?
-	
+
 	//link to web pages for information
-	  //Entertainment - http://www.marinaciviccenter.com/
-						//http://www.martintheatre.com/
-			//Visual Arts Center http://www.vac.org.cn/home.html  
-	
-	  //Hospitals - 
-		//Bay Med Map http://tinyurl.com/mdv6w7y
-	    //Gulf Medical Map http://tinyurl.com/l8nuaaa
-		
-	  //Shopping - 
-		//Pier Park(long link) http://tinyurl.com/pfhv888
-		//Wal-Mart on beach http://tinyurl.com/pfhv888
-	  
-	  //Lodging - Various Map locations of lodging http://tinyurl.com/pfhv888
-		
-	  //Restaurants - Various Restaurant locations http://tinyurl.com/kx5p8k2
-	
-	
+	//Entertainment - http://www.marinaciviccenter.com/
+	//http://www.martintheatre.com/
+	//Visual Arts Center http://www.vac.org.cn/home.html  
+
+	//Hospitals - 
+	//Bay Med Map http://tinyurl.com/mdv6w7y
+	//Gulf Medical Map http://tinyurl.com/l8nuaaa
+
+	//Shopping - 
+	//Pier Park(long link) http://tinyurl.com/pfhv888
+	//Wal-Mart on beach http://tinyurl.com/pfhv888
+
+	//Lodging - Various Map locations of lodging http://tinyurl.com/pfhv888
+
+	//Restaurants - Various Restaurant locations http://tinyurl.com/kx5p8k2
+
+
 	//main source for visitor information:  http://www.pcgov.org/visitors
-	
+
 	//various button categories that open more selections ( see document for categories.)  Possibly add 'about' section?
 
 	@Override
@@ -53,24 +55,27 @@ public class MainActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		entertainmentBtn = (Button) findViewById(R.id.entertainment);
 		entertainmentBtn.setOnClickListener(entertainmentListener);
-		
+
 		beachLocationsBtn = (Button) findViewById(R.id.beachLocations);
 		beachLocationsBtn.setOnClickListener(beachLocationsListener);
-		
-    restaurantsBtn = (Button) findViewById(R.id.restaurants);
-    restaurantsBtn.setOnClickListener(restaurantsListener);
-    
+
+		restaurantsBtn = (Button) findViewById(R.id.restaurants);
+		restaurantsBtn.setOnClickListener(restaurantsListener);
+
 		hotelsBtn = (Button) findViewById(R.id.hotels);
 		hotelsBtn.setOnClickListener(hotelsListener);
-		
+
 		hospitalsBtn = (Button) findViewById(R.id.hospitals);
 		hospitalsBtn.setOnClickListener(hospitalsListener);
-		
+
 		shoppingBtn = (Button) findViewById(R.id.shopping);
 		shoppingBtn.setOnClickListener(shoppingListener);
+
+		newsBtn = (Button) findViewById(R.id.buttonNews);
+		newsBtn.setOnClickListener(newsListener);
 	}
 
 	@Override
@@ -81,57 +86,67 @@ public class MainActivity extends Activity
 		return true;
 	}
 
-  OnClickListener entertainmentListener = new OnClickListener()
-  {
-    public void onClick(View v)
-    {
-      Intent intent = new Intent(getApplicationContext(), Entertainment.class);
-      startActivity(intent);
-    }
-  };
+	OnClickListener entertainmentListener = new OnClickListener()
+	{
+		public void onClick(View v)
+		{
+			WebView browser = new WebView(getApplicationContext());
+			setContentView(browser);
+			browser.loadUrl("http://www.pcgov.org/visitors/arts-and-entertainment");
+		}
+	};
 
-  OnClickListener beachLocationsListener = new OnClickListener()
-  {
-    public void onClick(View v)
-    {
-    }
-  };
+	OnClickListener beachLocationsListener = new OnClickListener()
+	{
+		public void onClick(View v)
+		{
+		}
+	};
 
-  OnClickListener hotelstListener = new OnClickListener()
-  {
-    public void onClick(View v)
-    {
-    }
-  };
-  
-  OnClickListener hotelsListener = new OnClickListener()
-  {
-    public void onClick(View v)
-    {
-    }
-  };
+	OnClickListener hotelstListener = new OnClickListener()
+	{
+		public void onClick(View v)
+		{
+		}
+	};
 
-  OnClickListener hospitalsListener = new OnClickListener()
-  {
-    public void onClick(View v)
-    {
-      Intent intent = new Intent(getApplicationContext(), Hospitals.class);
-      startActivity(intent);
-    }
-  };
+	OnClickListener hotelsListener = new OnClickListener()
+	{
+		public void onClick(View v)
+		{
+		}
+	};
 
-  OnClickListener restaurantsListener = new OnClickListener()
-  {
-    public void onClick(View v)
-    {
-    }
-  };
+	OnClickListener hospitalsListener = new OnClickListener()
+	{
+		public void onClick(View v)
+		{
+		}
+	};
 
-  OnClickListener shoppingListener = new OnClickListener()
-  {
-    public void onClick(View v)
-    {
-    }
-  };
+	OnClickListener restaurantsListener = new OnClickListener()
+	{
+		public void onClick(View v)
+		{
+		}
+	};
+
+	OnClickListener shoppingListener = new OnClickListener()
+	{
+		public void onClick(View v)
+		{
+		}
+	};
+
+	OnClickListener newsListener = new OnClickListener()
+	{
+		public void onClick(View v)
+		{
+            Intent myIntent = new Intent(v.getContext(), NewsRSSFeed.class);
+            startActivityForResult(myIntent, 0);
+		}
+
+
+	};
 
 }
