4bcae23718b7392ab3640b0b8ca9bd0df161ceb6
mastash3ff <bsheffield2008@yahoo.com>
2013-07-21 17:06:47 -0500

    CLeaned up NewsFeed.java

--- GroupProject/src/com/example/groupproject/NewsFeed.java
+++ GroupProject/src/com/example/groupproject/NewsFeed.java
@@ -3,16 +3,13 @@ package com.example.groupproject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import java.util.Stack;
 
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import java.util.StringTokenizer;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
@@ -21,73 +18,81 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.xml.sax.Attributes;
-import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class NewsFeed extends Activity {
-    /** Called when the activity is first created. */
-    String rssResult = "";
-    boolean item = false;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_news_feed);
-        
-        Button buttonPrevious = (Button) findViewById(R.id.ButtonPrevious);
-        buttonPrevious.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                setResult(RESULT_OK, intent);
-                finish();
-            }
+	/** Called when the activity is first created. */
+	String rssResult = "";
+	boolean item = false;
 
-        });
-        
-        
-        TextView rss = (TextView) findViewById(R.id.textViewNews);
-        try {
-            URL rssUrl = new URL("http://www.javaworld.com/index.xml");
-            SAXParserFactory factory = SAXParserFactory.newInstance();
-            SAXParser saxParser = factory.newSAXParser();
-            XMLReader xmlReader = saxParser.getXMLReader();
-            RSSHandler rssHandler = new RSSHandler();
-            xmlReader.setContentHandler(rssHandler);
-            InputSource inputSource = new InputSource(rssUrl.openStream());
-            xmlReader.parse(inputSource);
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_news_feed);
 
-        } catch (IOException e) {rss.setText(e.getMessage());
-        } catch (SAXException e) {rss.setText(e.getMessage());
-        } catch (ParserConfigurationException e) {rss.setText(e.getMessage());
-        }
+		Button buttonPrevious = (Button) findViewById(R.id.ButtonPrevious);
+		buttonPrevious.setOnClickListener(new View.OnClickListener() 
+		{
+			public void onClick(View view) 
+			{
+				Intent intent = new Intent();
+				setResult(RESULT_OK, intent);
+				finish();
+			}
 
-        rss.setText(rssResult);
-    }
+		});
 
-    private class RSSHandler extends DefaultHandler {
+		TextView rss = (TextView) findViewById(R.id.textViewNews);
+		try {
+			URL rssUrl = new URL("http://www.javaworld.com/index.xml");
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			SAXParser saxParser = factory.newSAXParser();
+			XMLReader xmlReader = saxParser.getXMLReader();
+			RSSHandler rssHandler = new RSSHandler();
+			xmlReader.setContentHandler(rssHandler);
+			InputSource inputSource = new InputSource(rssUrl.openStream());
+			xmlReader.parse(inputSource);
 
-        public void startElement(String uri, String localName, String qName,
-                Attributes attrs) throws SAXException {
-            if (localName.equals("item"))
-                item = true;
+		} 
+		catch (IOException e) {rss.setText(e.getMessage());
+		} 
+		catch (SAXException e) {rss.setText(e.getMessage());
+		} 
+		catch (ParserConfigurationException e) {rss.setText(e.getMessage());
+		}
 
-            if (!localName.equals("item") && item == true)
-                rssResult = rssResult + localName + ": ";
+		rss.setText(rssResult);
+	}
 
-        }
+	private class RSSHandler extends DefaultHandler
+	{
 
-        public void endElement(String namespaceURI, String localName,
-                String qName) throws SAXException {
+		public void startElement(String uri, String localName, String qName,
+				Attributes attrs) throws SAXException {
+			if (localName.equals("item"))
+				item = true;
 
-        }
+			if (!localName.equals("item") && item == true)
+				rssResult = rssResult + localName + ": ";
 
-        public void characters(char[] ch, int start, int length)
-                throws SAXException {
-            String cdata = new String(ch, start, length);
-            if (item == true)
-                rssResult = rssResult +(cdata.trim()).replaceAll("\\s+", " ")+"\t";
+		}
 
-        }
+		public void endElement(String namespaceURI, String localName,
+				String qName) throws SAXException
+				{
 
-    }
+				}
+
+		public void characters(char[] ch, int start, int length)
+				throws SAXException 
+				{
+			String cdata = new String(ch, start, length);
+			if (item == true)
+				rssResult = rssResult +(cdata.trim()).replaceAll("\\s+", " ")+"\t";
+
+				}
+
+	}
 }
