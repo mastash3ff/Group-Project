0566f49b2d3e5e405be85343ebfdc20d9089d2f7
Zeriam <derrickrb@comcast.net>
2013-07-20 21:24:56 -0500

    Reverting to working state.

--- GroupProject/gen/com/example/groupproject/R.java
+++ GroupProject/gen/com/example/groupproject/R.java
@@ -41,7 +41,6 @@ public final class R {
         public static final int activity_entertainment=0x7f030000;
         public static final int activity_hospitals=0x7f030001;
         public static final int activity_main=0x7f030002;
-        public static final int activity_news_rssfeed=0x7f030003;
     }
     public static final class menu {
         public static final int entertainment=0x7f070000;
